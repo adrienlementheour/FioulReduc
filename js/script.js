2807df9f18b719d5254925031ec87eb06fbf939b
Adrien <adrien.le.mentheour@gmail.com>
2014-08-20 12:03:56 +0200

    Bloc commande + animation

--- js/script.js
+++ js/script.js
@@ -2,9 +2,37 @@
 // variables //
 ///////////////
 
+/////////////////////////////////////////////////////////////////////////
+/////// FONCTION POUR OUVRIR ET FERMER LE BLOC COMMANDE DE LA HOME //////
+/////////////////////////////////////////////////////////////////////////
+function ouvertureFermetureBlocCommandeHome(type){
+	if (type=="ouverture") {
+		if (!$("#bloc-commande").hasClass("bloc-ouvert")){
+			$("#bloc-commande").addClass("bloc-ouvert");
+			TweenMax.to($("#masque-form-home"), 0.5, {height: "240px", ease:Cubic.easeInOut});
+		}
+	}else {
+		if (!$("#bloc-commande").hasClass("bloc-ouvert")){
+			$("#bloc-commande").addClass("bloc-ouvert");
+			TweenMax.to($("#masque-form-home"), 0.5, {height: "240px", ease:Cubic.easeInOut});
+		}else{
+			$("#bloc-commande").removeClass("bloc-ouvert");
+			TweenMax.to($("#masque-form-home"), 0.5, {height: "50px", ease:Cubic.easeInOut});
+		}
+	}
+}
 
 $(document).ready(function(){
-
+	if($("body").hasClass("home")){
+		$("a#fil-commande").click(function(){
+			ouvertureFermetureBlocCommandeHome();
+			return false;
+		});
+		$("#order-zipcode").focusin(function(){
+			ouvertureFermetureBlocCommandeHome("ouverture");
+			return false;
+		});
+	}
 });
 
 $( window ).resize(function() {
